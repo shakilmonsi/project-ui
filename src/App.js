43dfa37057160ff1bcca90cff97a1509a4a2ce41
unknown <samdshakilofficial@gmail.com>
2022-12-11 12:10:56 +0500

    add typrogtapu  nok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
+
+import { useEffect, useState } from "react";
+// import { LoadingButton } from "@mui/material";
 
 function App() {
+  const [submitting, setSubmitting] = useState(false);
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setSubmitting(false);
+    }, 1000);
+    return () => {
+      clearTimeout(id);
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        style={{
+          background: "#f7f7f7",
+
+          width: "400px",
+
+          height: "400px",
+
+          borderRadius: "1rem",
+          padding: "2rem",
+        }}
+      >
+        <h3>shakil monsi mama</h3>
+        <Button
+          variant="contained"
+          color="secondary"
+          fullWidth
+          sx={{
+            mb: 2,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Checkout
+        </Button>
+        <Typography variant="h1" gutterBottom>
+          h1. Heading
+        </Typography>
+        {/* <LoadingButton
+          loading={submitting}
+          variant="outlined"
+          onClick={() => setSubmitting(true)}
+        >
+          Fetch data
+        </LoadingButton> */}
+      </div>
     </div>
   );
 }
